7780ffaefd2f84b4f1b4c28576527bda8a3fd442
zjpiazza <zjpiazza@gmail.com>
2023-04-25 21:31:54 -0500

    Added count to application response

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+count = 0
+
 @app.route("/")
 def hello():
-    return "Hello World!!!"
+    count += 1
+    return f"Hello World!!! I have been called {count} times."
