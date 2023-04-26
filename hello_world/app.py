cd7f72feb394bfa34c80aa88ac760156ce5b728a
zjpiazza <zjpiazza@gmail.com>
2023-04-25 21:57:10 -0500

    Changed count to uppercase

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-count = 0
+COUNT = 0
 
 @app.route("/")
 def hello():
-    count += 1
-    return f"Hello World!!! I have been called {count} times."
+    COUNT += 1
+    return f"Hello World!!! I have been called {COUNT} times."
